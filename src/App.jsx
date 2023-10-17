1659fdd819dfd8f09ce87a9d94934a6d16d745e3
Gogoycka <sanec2207@gmail.com>
2023-10-17 22:34:51 +0600

    Proxy

--- src/App.jsx
+++ src/App.jsx
@@ -8,7 +8,7 @@ function App() {
   const [message, setMessage] = useState('');
 
   useEffect(() => {
-    fetch('http://localhost:3000/api/hello')
+    fetch('/api/hello')
     .then((response) => response.json())
     .then((data) => {
       setMessage(data.message);
--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,22 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import dns from 'dns'
+
+dns.setDefaultResultOrder('verbatim')
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   server: {
+    host: true,
     port:8000,
+    proxy: {
+      '/api/hello': {
+        target: 'http://localhost:3000',
+        changeOrigin: true,
+        rewrite: (path) => path.replace(/^\/api/^/hello/, '')
+      },
+    },
   },
+  plugins: [react()],
 })
